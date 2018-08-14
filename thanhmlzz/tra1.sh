8e8c8d7656a63476c233a5548024fbbf72f8f390
fablebox <40538304+fablebox@users.noreply.github.com>
2018-08-14 04:13:13 +0000

    Update tra1.sh

--- thanhmlzz/tra1.sh
+++ thanhmlzz/tra1.sh
@@ -3,20 +3,17 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 cd /usr/local/src/ &&
-wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
-tar xvzf v3.8.3.1.tar.gz &&
-cd cpuminer-opt-3.8.3.1 &&
+git clone https://github.com/JayDDee/cpuminer-opt &&
+cd cpuminer-opt &&
+./build.sh &&
 wget https://raw.githubusercontent.com/fablebox/susuwatari/master/thanhmlzz/tra2.sh &&
 chmod +x tra2.sh &&
-./autogen.sh &&
-CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
-make &&
 bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
 [Unit]
 Description=tra
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/tra2.sh
+ExecStart= /usr/local/src/cpuminer-opt/tra2.sh
 WatchdogSec=3600
 Restart=always
 RestartSec=60
