d54069478a0774038dc0e661e80a5d3f7f78cb41
fablebox <40538304+fablebox@users.noreply.github.com>
2018-09-01 03:43:54 +0000

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a CryptoNightV7 -o stratum+tcp://pool.minexmr.com:5555 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
+/usr/local/src/cpuminer-opt/cpuminer -a CryptoNightV7 -o stratum+tcp://pool.minexmr.com:5555 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x -b 0
