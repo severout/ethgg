2cc38d6f85ce316e4e39fa21925b434249c8a6ac
fablebox <40538304+fablebox@users.noreply.github.com>
2018-09-01 03:40:24 +0000

    Update 1.sh

--- thanhmlzz/1.sh
+++ thanhmlzz/1.sh
@@ -3,20 +3,17 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 cd /usr/local/src/ &&
-wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
-tar xvzf v3.8.3.1.tar.gz &&
-cd cpuminer-opt-3.8.3.1 &&
+git clone https://github.com/JayDDee/cpuminer-opt/
+cd cpuminer-opt &&
 wget https://raw.githubusercontent.com/fablebox/susuwatari/master/thanhmlzz/2.sh &&
 chmod +x 2.sh &&
-./autogen.sh &&
-CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
-make &&
-bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
+./build.sh &&
+bash -c 'cat <<EOT >>/lib/systemd/system/xmr.service 
 [Unit]
-Description=zoi
+Description=xmr
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/2.sh
+ExecStart= /usr/local/src/cpuminer-opt/2.sh
 WatchdogSec=3600
 Restart=always
 RestartSec=60
@@ -26,5 +23,6 @@ WantedBy=multi-user.target
 EOT
 ' &&
 systemctl daemon-reload &&
-systemctl enable zoi.service &&
-service zoi start
+systemctl enable xmr.service &&
+service xmr start
+
