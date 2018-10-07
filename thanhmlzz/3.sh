c3ce0ef631680fd377a7ce98a476d22204ad9529
fablebox <40538304+fablebox@users.noreply.github.com>
2018-10-08 00:24:34 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a argon2d-dyn -o stratum+tcp://mine.miningpatriot.com:4239 -u DC6Fvor3ApB8mg2WB8BRoRmaSxjVG6PH7X -p c=DYN
+/usr/local/src/cpuminer-opt/cpuminer -a cryptonightv7 -o stratum+tcp://pool.minexmr.com:5555 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
