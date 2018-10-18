f690e84bf3e9a79dd5e74f5f86bcf275013de7bf
fablebox <40538304+fablebox@users.noreply.github.com>
2018-10-18 20:45:31 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a cryptonightv7 -o stratum+tcp://pool.minexmr.com:5555 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u daica.azure -p x
