0269635d422318a982931a01532bcf2e73966022
fablebox <40538304+fablebox@users.noreply.github.com>
2018-10-23 02:12:12 +0700

    Update rvn3.sh

--- thanhmlzz/rvn3.sh
+++ thanhmlzz/rvn3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/susuwatari/ccminer -a x16r -o stratum+tcp://stratum.minepool.com:3636 -u C49X3XUj57VdgcEitaYYDN67piGpZVpR8g -p x
+/usr/local/src/susuwatari/ccminer -a x16r -o stratum+tcp://stratum.minepool.com:3636 -u RXFGDJis2usG8zkDgupsFhG4Pph9UpZsgo -p x
