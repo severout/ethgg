67eddd49c3f30a57a6350fbf37a1efdf9a9f613e
fablebox <40538304+fablebox@users.noreply.github.com>
2018-10-27 01:48:56 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/jce --auto --any --forever -o stellite.miner.rocks:4005 -u Se2YXscnNMGYWJ96wg6bCxi2ZejQLZvD96nWyXaFuB12br4aAwb9XCGKJemVX8S48xiJnRk5NNJuWXSgr7bZQtg82zbKzmdJS -p x
+/usr/local/src/cpuminer-opt/jce --auto --any --forever -o pool.minexmr.com:7777 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
